e581fa38a8a2c1460eb826c0ca3be7f37f481f54
Yukich <sevighnsit@gmail.com>
2015-05-25 18:58:37 +0900

    first commit

---
+++ twiruby.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby
+
+require 'twitter'
+require "./config.rb"
+include Client
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key     = read_consumer_key()
+  config.consumer_secret  = read_consumer_secret()
+end
+
+# def collect_with_max_id(collection=[], max_id=nil, &block)
+#   response = yield(max_id)
+#   collection += response
+#   response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+# end
+#
+# def client.get_all_tweets(user)
+#   collect_with_max_id do |max_id|
+#     options = {count: 20, include_rts: true}
+#     options[:max_id] = max_id unless max_id.nil?
+#     user_timeline(user, options)
+#   end
+# end
+#
+# client.get_all_tweets("mtjune11")
+
+client.search("to:mtjune11", result_type: "recent").take(3).each do |tweet|
+  puts tweet.text
+end
