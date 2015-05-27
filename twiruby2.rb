be0d2f375413c6118e38340586f6c8fb02af233b
mtjune <mtjune@v157-7-204-174.z1d5.static.cnode.jp>
2015-05-27 09:23:21 +0900

    test streaming code

--- twiruby2.rb
+++ twiruby2.rb
@@ -1,8 +1,22 @@
 #!/usr/bin/ruby
 
-
+require "tweetstream"
 require "./config.rb"
 include Client
 
+keys = get_keys()
 
-uri="https://api.twitter.com/1.1/search/tweets.json"
+TweetStream.configure do |config|
+	config.consumer_key = keys["consumer_key"]
+	config.consumer_secret = keys["consumer_secret"]
+	config.oauth_token = keys["access_token"]
+	config.oauth_token_secret = keys["access_token_secret"]
+	config.auth_method = :oauth
+end
+
+
+client = TweetStream::Client.new
+client.userstream do |status|
+	puts " #{status.user.name} -> #{status.text}\n\n"
+end
+
