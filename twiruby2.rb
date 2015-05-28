179648d2fd1764bc40c49193dfe16670f269aeff
Yukich <sevighnsit@gmail.com>
2015-05-28 19:34:04 +0900

    dev

--- twiruby2.rb
+++ twiruby2.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'twitter'
+require 'mongo'
 require "./config.rb"
 include Twikeys
 
@@ -18,7 +19,6 @@ end
 #   print object.user.screen_name + " -> " + object.text + "\n" if object.is_a?(Twitter::Tweet)
 # end
 
-
 client.sample do |object|
   if object.is_a?(Twitter::Tweet)
     if /^[^(RT)].+(食べたい|たべたい)/ =~ object.text
@@ -26,3 +26,36 @@ client.sample do |object|
     end
   end
 end
+
+
+class Tweet_Collector
+  def initialize()
+    @client = Twitter::Streaming::Client.new do |config|
+			config.consumer_key = Twikeys::Consumer_key
+			config.consumer_secret = Twikeys::Consumer_secret
+			config.access_token = Twikeys::Access_token
+			config.access_token_secret = Twikeys::Access_token_secret
+    end
+
+    connection = Mongo::Connection.new
+    db = connection.db('twitter_jp')
+    @jp_col = db.collection('twitter_jp')
+
+    db = connection.db('twitter_nojp')
+    @nojp_col = db.collection('twitter_nojp')
+  end
+
+  def start
+    @client.sample do |status|
+      if /[ぁ-んァ-ヴ]+/u =~ status.text then
+        @jp_col.insert(status.to_h)
+      else
+        @nojp_col.insert(status.to_h)
+      end
+    end
+  end
+end
+
+
+collector = Twitter_Collector.new()
+collector.start
